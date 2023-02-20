de9dc5ecf90e1a0ee500343cf46c5d7672344467
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-20 12:49:37 -0800

    Added basic code to blink the two leds on the board. Note: I'm using a for loop as delay for the moment instead of use a timer.

--- Blink/main.c
+++ Blink/main.c
@@ -2,7 +2,18 @@
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
+    int i=0;
+	WDTCTL = WDTPW | WDTHOLD;	        // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;               // Disable the GPIO power-on default high-impedance mode
+
+    P1DIR |= (1<<0);
+    P4DIR |= (1<<0);
+
+    while(1)
+    {
+        P4OUT   ^= (1<<0);
+        P1OUT   ^= (1<<0);
+        for(i=0; i<30000;i++);
+    }
 	return 0;
 }
